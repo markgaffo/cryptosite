c6dad9e55629b3e2d04fe6df7a9e04b2b4db2d36
markgaffo <markgaffo@yahoo.ie>
2021-03-14 00:54:36 +0000

    Coin and News displayed on page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,12 @@ class HomeController < ApplicationController
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
     
+    #reading crypto coin vaule data
+    @coin_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,LTC,ZRX,OMG,XLM,BCH&tsyms=EUR'
+    @coin_uri = URI(@coin_url)
+    @coin_response = Net::HTTP.get(@coin_uri)
+    @coin_price = JSON.parse(@coin_response)
+    
   end
   
   def prices
