ff7c29892cb11ef4deb7825da8596a3c05ab80b2
markgaffo <markgaffo@yahoo.ie>
2021-03-16 03:16:43 +0000

    start of signup new gem for bootstrap needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   root 'home#index'
   get 'home/prices'
   
   post "/home/prices" => 'home/prices'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
