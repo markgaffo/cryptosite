a0b56258c91a870a1a1fc9f766a7741e4915a89c
markgaffo <markgaffo@yahoo.ie>
2021-03-19 23:00:49 +0000

    updated profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   
   resources :profiles
   resources :balances
+  
+  
   devise_for :users
   root 'home#index'
   get 'home/prices'
   
   post "/home/prices" => 'home/prices'
+  
+  get '/signedinuserprofile' => 'profiles#signedinuserprofile'
+  
 end
